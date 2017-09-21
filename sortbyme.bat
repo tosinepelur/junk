7d748eb53cfc2743dabdc6d2ca76e7f9c0cff1c7
tosinepelur <tosinepelur@gmail.com>
2017-09-21 12:57:54 +0300

    Added autoinstall; launcher.
    Added an auto install of sortbyme.sh, added creation of file launch-sortbyme.bat for easier start.

--- sortbyme.bat
+++ sortbyme.bat
@@ -1,4 +1,40 @@
-echo msgbox "You must download & configure bash.exe to run this script. Check bit.ly/sortbyme" > %tmp%\tmp.vbs
+@echo off
+echo msgbox "You must download & configure bash.exe to run this script. Check http://bit.ly/sortbyme for a guide. Click 'OK' to proceed downloading sortbyme.sh" > %tmp%\tmp.vbs
 wscript %tmp%\tmp.vbs
 del %tmp%\tmp.vbs
+
+@echo off
+set DLOAD_SCRIPT=download.vbs
+echo Option Explicit                                                    >  %DLOAD_SCRIPT%
+echo Dim args, http, fileSystem, adoStream, url, target, status         >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo Set args = Wscript.Arguments                                       >> %DLOAD_SCRIPT%
+echo Set http = CreateObject("WinHttp.WinHttpRequest.5.1")              >> %DLOAD_SCRIPT%
+echo url = args(0)                                                      >> %DLOAD_SCRIPT%
+echo target = args(1)                                                   >> %DLOAD_SCRIPT%
+echo WScript.Echo "Getting '" ^& target ^& "' from '" ^& url ^& "'..."  >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo http.Open "GET", url, False                                        >> %DLOAD_SCRIPT%
+echo http.Send                                                          >> %DLOAD_SCRIPT%
+echo status = http.Status                                               >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo If status ^<^> 200 Then                                            >> %DLOAD_SCRIPT%
+echo    WScript.Echo "FAILED to download: HTTP Status " ^& status       >> %DLOAD_SCRIPT%
+echo    WScript.Quit 1                                                  >> %DLOAD_SCRIPT%
+echo End If                                                             >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo Set adoStream = CreateObject("ADODB.Stream")                       >> %DLOAD_SCRIPT%
+echo adoStream.Open                                                     >> %DLOAD_SCRIPT%
+echo adoStream.Type = 1                                                 >> %DLOAD_SCRIPT%
+echo adoStream.Write http.ResponseBody                                  >> %DLOAD_SCRIPT%
+echo adoStream.Position = 0                                             >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo Set fileSystem = CreateObject("Scripting.FileSystemObject")        >> %DLOAD_SCRIPT%
+echo If fileSystem.FileExists(target) Then fileSystem.DeleteFile target >> %DLOAD_SCRIPT%
+echo adoStream.SaveToFile target                                        >> %DLOAD_SCRIPT%
+echo adoStream.Close                                                    >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+cscript //Nologo %DLOAD_SCRIPT% https://raw.githubusercontent.com/tosinepelur/junk/master/sortbyme.sh sortbyme.sh
+del download.vbs
+echo start cmd.exe /k "bash.exe sortbyme.sh" > launch-sortbyme.bat
 start cmd.exe /k "bash.exe sortbyme.sh"
